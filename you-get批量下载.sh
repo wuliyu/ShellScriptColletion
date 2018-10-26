f9112d54a53485afff34ab4bffc6b772bdd6fee8
wuliyu <30580363+wuliyu@users.noreply.github.com>
2018-10-26 14:56:44 +0800

    Add files via upload

---
+++ you-get批量下载.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+download(){
+	urls=$1
+	for url in ${urls[@]};do
+		you-get ${url}
+	done
+}
+urls=(http://www.iqiyi.com/v_19rrbbhz0g.html http://www.iqiyi.com/v_19rrbbixyg.html http://www.iqiyi.com/v_19rrbbtqeg.html http://www.iqiyi.com/v_19rrbbxcco.html http://www.iqiyi.com/v_19rrbaygfc.html http://www.iqiyi.com/v_19rrbazah0.html http://www.iqiyi.com/v_19rrbbfazo.html http://www.iqiyi.com/v_19rrbbfy1g.html http://www.iqiyi.com/v_19rrb9orbg.html http://www.iqiyi.com/v_19rrb9qzec.html http://www.iqiyi.com/v_19rrb97xtg.html http://www.iqiyi.com/v_19rrb8i1s4.html http://www.iqiyi.com/v_19rrb8yot8.html http://www.iqiyi.com/v_19rrb8z41o.html http://www.iqiyi.com/v_19rrax6ih0.html http://www.iqiyi.com/v_19rrax5864.html http://www.iqiyi.com/v_19rrawq0lo.html http://www.iqiyi.com/v_19rrawqid0.html http://www.iqiyi.com/v_19rraw9098.html http://www.iqiyi.com/v_19rraw9vak.html http://www.iqiyi.com/v_19rravnuno.html http://www.iqiyi.com/v_19rravo5gs.html http://www.iqiyi.com/v_19rrav8ku4.html http://www.iqiyi.com/v_19rraujyj8.html http://www.iqiyi.com/v_19rrats3b0.html http://www.iqiyi.com/v_19rrb38y3w.html http://www.iqiyi.com/v_19rrb3ce3g.html http://www.iqiyi.com/v_19rrb1l364.html http://www.iqiyi.com/v_19rrb1lnwc.html http://www.iqiyi.com/v_19rrb157vw.html http://www.iqiyi.com/v_19rrb18kz8.html http://www.iqiyi.com/v_19rrb0ru4c.html http://www.iqiyi.com/v_19rrb0sfo4.html http://www.iqiyi.com/v_19rraz0zpw.html)
+download ${urls}
